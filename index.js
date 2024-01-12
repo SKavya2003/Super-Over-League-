a2471aa8b17325e0d84a1d8d9f2a7802c237d6b3
Kavya-Sree-2003 <sankulakavyasree2003@gmail.com>
2024-01-12 13:40:04 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        className='image'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        className='image'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
