9946b461d170d73a9dfd9d01e4764a18adf8b011
SANKULA KAVYA SREE <153528702+Kavya-Sree-2003@users.noreply.github.com>
2024-01-13 14:11:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const element = (
-  <div className='container'>
-    <h1 className='heading'>Super Over League</h1>
+//   Write your code here.
+ <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
     <div>
       <img
-        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
-        className='image'
+        className="first-image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
       />
       <img
-        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
-        className='image'
+        className="second-image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
       />
     </div>
-  </div>
+  </div> //   Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
 )
 
-ReactDOM.render(element, document.getElementById('root'))
